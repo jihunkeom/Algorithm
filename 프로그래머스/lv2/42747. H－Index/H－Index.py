9cf73f27b87957ce58dbf43e8ec7451db73b6087
Ji Hun Keom <samkeom@korea.ac.kr>
2023-04-12 22:32:47 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,15 +1,16 @@
 def solution(citations):
     answers = []
     citations = sorted(citations, reverse=True)
-    if citations[0] == 0:
-        return 0
-    for i in range(1, len(citations)+1):
-        answer = 0
-        for paper in citations:
-            if paper >= i:
-                answer += 1
-            if answer >= i:
-                answers.append(answer)
-                break
+    return max(map(min, enumerate(citations, 1)))
+#     if citations[0] == 0:
+#         return 0
+#     for i in range(1, len(citations)+1):
+#         answer = 0
+#         for paper in citations:
+#             if paper >= i:
+#                 answer += 1
+#             if answer >= i:
+#                 answers.append(answer)
+#                 break
 
-    return max(answers)
+#     return max(answers)
