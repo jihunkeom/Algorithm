b8aed5f18f824b4cc2ce68c3249de2019c104c4d
Ji Hun Keom <samkeom@korea.ac.kr>
2023-04-15 15:13:44 +0900

    [Gold IV] Title: 오큰수, Time: 1684 ms, Memory: 194216 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+NGE = [-1] * n
+stack = []
+
+for i in range(n):
+    x = arr[i]
+    if len(stack) == 0 or stack[-1][0] >= x:
+        stack.append((x, i))
+    else:
+        while len(stack) > 0:
+            previous, index = stack.pop()
+            if previous >= x:
+                stack.append((previous, index))
+                break
+            else:
+                NGE[index] = x
+        stack.append((x, i))
+        
+for x in NGE:
+    print(x, end=" ")
