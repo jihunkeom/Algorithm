2e61babdcbf41716bb998b0df9dd5ca40f4e27bb
Ji Hun Keom <samkeom@korea.ac.kr>
2023-04-16 15:45:01 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+    answer = [0] * n
+    answer[0] = 1
+    answer[1] = 2
+    for i in range(2, n):
+        answer[i] = answer[i-1]+answer[i-2]
+    
+    return answer[n-1] % 1234567
