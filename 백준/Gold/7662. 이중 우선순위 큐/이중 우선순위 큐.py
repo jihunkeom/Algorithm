ab049d22b15d03249463094bc1cda8790373296d
Ji Hun Keom <samkeom@korea.ac.kr>
2023-04-19 14:00:51 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 7228 ms, Memory: 293808 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,33 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+def pop(heap):
+    while len(heap) > 0:
+        data, id = heapq.heappop(heap)
+        if not deleted[id]:
+            deleted[id] = True
+            return data
+    return None
+
+for _ in range(int(input())):
+    k = int(input())
+    min_heap = []
+    max_heap = []
+    current = 0
+    deleted = [False] * (k+1)
+    for i in range(k):
+        command = input().split()
+        operator, data = command[0], int(command[1])
+        if operator == "D":
+            if data == -1: pop(min_heap)
+            elif data == 1: pop(max_heap)
+        elif operator == "I":
+            heapq.heappush(min_heap, (data, current))
+            heapq.heappush(max_heap, (-data, current))
+            current += 1
+    max_value = pop(max_heap)
+    if max_value == None: print("EMPTY")
+    else:
+        heapq.heappush(min_heap, (-max_value, current))
+        print(-max_value, pop(min_heap))
