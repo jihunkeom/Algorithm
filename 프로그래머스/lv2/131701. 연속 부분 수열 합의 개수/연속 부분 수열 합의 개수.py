75b574710a2031328e80e98707c627e97f8b3584
Ji Hun Keom <samkeom@korea.ac.kr>
2023-04-20 19:20:44 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 5280.26 ms, Memory: 43.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -3,7 +3,7 @@ def solution(elements):
     elements *= 2
     answer = set([x for x in elements])
     for i in range(2, elem_num+1):
-        for j in range(len(elements)):
+        for j in range(elem_num):
             num = sum(elements[j:j+i])
             answer.add(num)
             
