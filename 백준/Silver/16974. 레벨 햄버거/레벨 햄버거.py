c6dd5c59de05e71482a34ba940aa78e4db301996
Ji Hun Keom <samkeom@korea.ac.kr>
2023-08-07 21:40:54 +0900

    [Silver I] Title: 레벨 햄버거, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/16974. 레벨 햄버거/레벨 햄버거.py
+++ 백준/Silver/16974. 레벨 햄버거/레벨 햄버거.py
@@ -1,26 +1,25 @@
 N, X = map(int, input().split())
 
-burger = [1] * 51            # 버거의 레이아웃 갯수
-patty = [1] * 51             # 버거의 패티 갯수
-
+burger = [1] * 51
+patty = [1] * 51 
 
 for i in range(1, N+1):
     burger[i] = 1 + burger[i-1] + 1 + burger[i-1] + 1
     patty[i] = patty[i-1] + 1 + patty[i-1]
-
-
-def eat(n, x):                      # 먹은 패티 갯수
+    
+def go(n, x):
     if n == 0:
         return x
     if x == 1:
         return 0
-    elif x <= 1 + burger[n-1]:						# case 1
-        return eat(n-1, x-1)                # 맨 아래 번 빼고 (x-1)
-    elif x == 1 + burger[n-1] + 1:                  # case 2 딱 ~가운데 패티까지 먹은 경우
+    
+    if x == (burger[n-1] + 2):
         return patty[n-1] + 1
-    elif x <= burger[n-1] + burger[n-1] + 1 + 1:	# case 3
-        return patty[n-1] + 1 + eat(n-1, (x-(burger[n-1]+2)))
-    else:											# case 4
+    elif x <= burger[n-1] + 1:
+        return go(n-1, x-1)
+    elif x <= 2*burger[n-1] + 2:
+        return patty[n-1] + 1 + go(n-1, (x-(burger[n-1]+2)))
+    else:
         return patty[n]
-
-print(eat(N, X))
+    
+print(go(N, X))
