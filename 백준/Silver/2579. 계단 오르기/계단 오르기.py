649f3c6f9cbb386a4a1188afe0bd2c06502461bd
Ji Hun Keom <samkeom@korea.ac.kr>
2024-02-20 15:13:26 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,11 +1,18 @@
-n=int(input()) # 계단 개수
-s=[int(input()) for _ in range(n)] # 계단 리스트
-dp=[0]*(n) # dp 리스트
-if len(s)<=2: # 계단이 2개 이하일땐 그냥 다 더해서 출력
-    print(sum(s))
-else: # 계단이 3개 이상일 때
-    dp[0]=s[0] # 첫째 계단 수동 계산
-    dp[1]=s[0]+s[1] # 둘째 계단까지 수동 계산
-    for i in range(2,n): # 3번째 계단 부터 dp 점화식 이용해서 최대값 구하기
-        dp[i]=max(dp[i-3]+s[i-1]+s[i], dp[i-2]+s[i])
+import sys
+
+n = int(sys.stdin.readline())
+stairs = []
+dp = [0] * (n)
+for _ in range(n):
+    tmp = int(sys.stdin.readline())
+    stairs.append(tmp)
+
+if n < 3:
+    print(sum(stairs))
+else:
+    dp[0] = stairs[0]
+    dp[1] = stairs[0] + stairs[1]
+
+    for i in range(2, n):
+        dp[i] = max(dp[i-3] + stairs[i-1] + stairs[i], dp[i-2] + stairs[i])
     print(dp[-1])
