2ada0485fa46ffd3e46bb2a2a8dc2e7a1658d74d
Ji Hun Keom <samkeom@korea.ac.kr>
2024-02-24 16:36:49 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10920 ms, Memory: 174492 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,38 +1,34 @@
-from collections import deque
 import sys
+from collections import deque
 
-input = sys.stdin.readline
+n, m = map(int, sys.stdin.readline().split())
+computers = [[] for _ in range(n+1)]
 
-# bfs
-def graph(n):
-    queue = deque([n])
-    cnt = 0
-    visited = [0] * (N+1)
-    visited[n] = 1
+for _ in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    computers[b].append(a)
+    
+counts = [0] * (n+1)
+max_cnt = 0
+for x in range(1, n+1):
+    visited = [0] * (n+1)
+    count = 0
+    queue = deque([x])
+    visited[x] = 1
+    
     while queue:
-        q = queue.popleft()
-        cnt += 1
-        # 해킹가능한 컴퓨터가 존재하지 않을 때 까지 탐색
-        for i in comArr[q]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = 1
-    return cnt
-
-N, M = map(int, input().split())
-comArr = [[] for _ in range(N+1)]
-
-# 컴퓨터의 A 와 B 관계를 2차원 배열로 정리
-for _ in range(M):
-    A, B = map(int, input().split())
-    comArr[B].append(A)
-
-# 각 컴퓨터마다 최대 해킹 가능한 컴퓨터의 수 저장
-answer = [0 for _ in range(N+1)]
-for i in range(1, N+1):
-    answer[i] = graph(i)
-
-# 해킹 가능한 컴퓨터가 가장 많은 컴퓨터의 번호 출력
-for i in range(1, N+1):
-    if answer[i] == max(answer):
-        print(i, end=' ')
+        pos = queue.popleft()
+        count += 1
+            
+        for nx in computers[pos]:
+            if visited[nx] == 0:
+                queue.append(nx)
+                visited[nx] = 1
+                
+    counts[x] = count
+    if count > max_cnt:
+        max_cnt = count
+    
+for i in range(1, n+1):
+    if counts[i] == max_cnt:
+        print(i, end=" ")
