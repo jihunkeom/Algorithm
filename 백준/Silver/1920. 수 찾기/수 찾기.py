0d7391e1ecd7ad64070a92ed6f8d4a5406637839
Ji Hun Keom <samkeom@korea.ac.kr>
2024-02-25 23:37:17 +0900

    [Silver IV] Title: 수 찾기, Time: 168 ms, Memory: 50480 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,14 +1,11 @@
-num = int(input())
-data_list = list(map(int, input().split()))
-num2 = int(input())
-data_list2 = list(map(int, input().split()))
-
-num_set = set()
-for x in data_list:
-    num_set.add(x)
-
-for x in data_list2:
-    if x in num_set:
-        print("1")
+import sys
+n = int(sys.stdin.readline())
+arr1 = list(map(int, sys.stdin.readline().split()))
+m = int(sys.stdin.readline())
+arr2 = list(map(int, sys.stdin.readline().split()))
+arr1 = set(arr1)
+for x in arr2:
+    if x in arr1:
+        print(1)
     else:
-        print("0")
+        print(0)
