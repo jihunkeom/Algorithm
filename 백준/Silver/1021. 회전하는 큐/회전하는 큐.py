86d5b4b0ab46d12674405e8ec7befc51df505235
Ji Hun Keom <samkeom@korea.ac.kr>
2024-02-28 09:17:53 +0900

    [Silver III] Title: 회전하는 큐, Time: 64 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -1,22 +1,25 @@
 import sys
 from collections import deque
-input = sys.stdin.readline
-n, idx = list(map(int, input().split()))
-idx_list = list(map(int, input().split()))
-dq = deque(list(range(1, n+1)))
 
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+queue = deque(list(range(1, n+1)))
 cnt = 0
-for i in idx_list:
-    while True:
-        if dq[0] == i:
-            dq.popleft()
-            break
-        else:
-            if dq.index(i) < len(dq)/2:
-                dq.append(dq.popleft())
-                cnt += 1
-            else:
-                dq.appendleft(dq.pop())
-                cnt += 1
-                
+
+for x in arr:
+    q, r = divmod(len(queue), 2)
+    if queue.index(x)+1 <= (q+1):
+        while queue[0] != x:
+            queue.append(queue.popleft())
+            cnt += 1
+    else:
+        while queue[0] != x:
+            queue.appendleft(queue.pop())
+            cnt += 1
+    queue.popleft()
+    
+        
 print(cnt)
