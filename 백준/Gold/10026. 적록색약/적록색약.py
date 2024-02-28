3e7bf896e48d00fe6fca9b2e495eb54c60ff6c1e
Ji Hun Keom <samkeom@korea.ac.kr>
2024-02-28 11:54:01 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 32508 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,48 +1,47 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
-n = int(input())
-map = list()
-
+n = int(input().rstrip())
+map1 = []
 for _ in range(n):
-    x = sys.stdin.readline().strip()
-    map.append(list(x))
+    map1.append([x for x in input().rstrip()])
 
-def bfs(map, start, who):
-    flag = False
-    queue = deque([start])
-    while queue:
-        r, c = queue.popleft()
-        color = map[r][c]
-        for next_r, next_c in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
-            if (0 <= next_r < n) and (0 <= next_c < n):
-                if who == 1:
-                    flag = not visited[next_r][next_c] and color == map[next_r][next_c]
-                else:
-                    flag = not visited[next_r][next_c] and ((color in ["R", "G"] and map[next_r][next_c] in ["R", "G"]) or (color == "B" and map[next_r][next_c] == "B"))
-                    
-                
-                if flag:
-                    visited[next_r][next_c] = True
-                    queue.append((next_r, next_c))
-    return map
+def dfs1(x, y):
+    for nx, ny in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+        if (nx < 0) or (ny < 0) or (nx >= len(map1)) or (ny >= len(map1[0])):
+            continue
+        if visited[nx][ny] == False and map1[nx][ny] == map1[x][y]:
+            visited[nx][ny] = True
+            dfs1(nx, ny)
+    return None
 
-cnt1 = 0
-visited = [[False for _ in range(n)] for _ in range(n)]
+def dfs2(x, y):
+    for nx, ny in ((x+1, y), (x-1, y), (x, y+1), (x, y-1)):
+        if (nx < 0) or (ny < 0) or (nx >= len(map1)) or (ny >= len(map1[0])):
+            continue
+        if visited[nx][ny] == False:
+            if (map1[nx][ny] in ["R", "G"] and map1[x][y] in ["R", "G"]) or (map1[nx][ny] == map1[x][y]):
+                visited[nx][ny] = True
+                dfs2(nx, ny)
+    return None
+
+visited = [[False] * n for _ in range(n)]
+answer1 = 0
 for i in range(n):
-    while not all(visited[i]):
-        start = (i, visited[i].index(False))
-        visited[start[0]][start[1]] = True
-        map = bfs(map, start, 1)
-        cnt1 += 1
-
-cnt2 = 0
-visited = [[False for _ in range(n)] for _ in range(n)]
+    for j in range(n):
+        if visited[i][j] == False:
+            visited[i][j] = True
+            dfs1(i, j)
+            answer1 += 1
+            
+visited = [[False] * n for _ in range(n)]
+answer2 = 0
 for i in range(n):
-    while not all(visited[i]):
-        start = (i, visited[i].index(False))
-        visited[start[0]][start[1]] = True
-        map = bfs(map, start, 2)
-        cnt2 += 1
-
-print(cnt1, cnt2)
+    for j in range(n):
+        if visited[i][j] == False:
+            visited[i][j] = True
+            dfs2(i, j)
+            answer2 += 1
+            
+print(answer1, answer2)
