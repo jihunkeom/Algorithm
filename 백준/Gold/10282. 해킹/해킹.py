4655e4c2a8d2a2d89d274c81ef0d534ef2b72265
Ji Hun Keom <samkeom@korea.ac.kr>
2024-03-17 19:25:17 +0900

    [Gold IV] Title: 해킹, Time: 812 ms, Memory: 48548 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.py
+++ 백준/Gold/10282. 해킹/해킹.py
@@ -3,31 +3,39 @@ import heapq
 
 input = sys.stdin.readline
 INF = int(1e9)
-
 t = int(input())
+
 for _ in range(t):
     n, d, c = map(int, input().split())
-    graph = [[] for _ in range(n+1)]
-    for j in range(d):
-        a, b, s = map(int, input().split())
-        graph[b].append((a,s))
     
+    graph = [[] for _ in range(n+1)]
     distances = [INF] * (n+1)
-    queue = []
-    heapq.heappush(queue, (0, c))
+    for _ in range(d):
+        x, y, z = map(int, input().split())
+        graph[y].append((x, z))
+        
+    heap_data = []
+    heapq.heappush(heap_data, (0, c))
     distances[c] = 0
-    while queue:
-        dist, node = heapq.heappop(queue)
-        # print(distances, node, dist)
-        if distances[node] > dist:
+    while heap_data:
+        dist, now = heapq.heappop(heap_data)
+        
+        if dist > distances[now]:
             continue
         
-        for next_node, next_dist in graph[node]:
-            # print("+", next_node, next_dist)
-            cost = dist + next_dist
-            if cost < distances[next_node]:
-                distances[next_node] = cost
-                heapq.heappush(queue, (cost, next_node))
+        for a, b in graph[now]:
+            cost = b + distances[now]
+            
+            if cost < distances[a]:
+                distances[a] = cost
+                heapq.heappush(heap_data, (cost, a))
                 
-    tmp = [x for x in distances if x < INF]
-    print(len(tmp), max(tmp))
+    answer_1 = 0
+    answer_2 = 0
+    for x in distances[1:]:
+        if x < INF:
+            answer_1 += 1
+
+            answer_2 = max(answer_2, x)
+    
+    print(answer_1, answer_2)
