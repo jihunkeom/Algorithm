87be228d41a39a9530eb76548c8fae28a94a77ce
Ji Hun Keom <samkeom@korea.ac.kr>
2024-03-22 18:01:10 +0900

    [Silver V] Title: 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,14 +1,22 @@
-line = input()
+import sys
+
+input = sys.stdin.readline
+
+arr = list(map(int, input().rstrip()))
 
 cnt0, cnt1 = 0, 0
-cur = "a"
-for x in line:
-    if x != cur:
-        if x == "1":
-            cnt1 += 1
-            cur = "1"
-        else:
+cur = arr[0]
+if cur == 0:
+    cnt0 += 1
+else:
+    cnt1 += 1
+
+for x in arr[1:]:
+    if cur != x:
+        cur = x
+        if x == 0:
             cnt0 += 1
-            cur = "0"
+        else:
+            cnt1 += 1
             
 print(min(cnt0, cnt1))
